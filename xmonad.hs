d75144ca9c5c155850015fe5d828abdbe9c9c3fa
Parv Mor <parvmor.pm@gmail.com>
2019-09-22 01:03:17 +0200

    Update configs.
    Signed-off-by: Parv Mor <parvmor.pm@gmail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -1,144 +1,174 @@
-import qualified Data.Map as M
-import           Graphics.X11.ExtraTypes.XF86
-import           System.Exit
-import           System.IO
-import           XMonad
-import           XMonad.Actions.SwapWorkspaces (swapWithCurrent)
-import           XMonad.Hooks.DynamicLog
-import qualified XMonad.Hooks.EwmhDesktops as E
-import           XMonad.Hooks.ManageDocks
-import           XMonad.Hooks.ManageHelpers
-import           XMonad.Hooks.SetWMName
-import           XMonad.Layout.Fullscreen
-import           XMonad.Layout.NoBorders
-import           XMonad.Layout.SimplestFloat
-import           XMonad.Layout.Spiral
-import           XMonad.Layout.Tabbed
-import           XMonad.Layout.ThreeColumns
-import           XMonad.Util.EZConfig(additionalKeys)
-import           XMonad.Util.Run(spawnPipe)
+import Data.Default
+
+import System.Exit
+import System.IO
+
+import XMonad hiding ((|||))
+
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
-myTerminal    = "/usr/bin/gnome-terminal"
-myWorkspaces  = ["1:web", "2:code", "3:SoH(Haskell)", "4:Tipsy(Scala)", "5:Competitive"] ++ map show [6..9]
-myScreensaver = "/usr/bin/xscreensaver-command --lock"
-myLauncher    = "$(yeganesh -x -- -fn '-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*' -nb '#000000' -nf '#FFFFFF' -sb '#7C7C7C' -sf '#CEFFAC')"
+import XMonad.Actions.GroupNavigation
+import XMonad.Actions.PhysicalScreens
 
-myManageHook = composeAll
-    [ className =? "chromium"    --> doShift "1:web"
-    , className =? "VirtualBox"  --> doShift "3:vm"
-    , className =? "stalonetray" --> doIgnore
-    , className =? "vlc"         --> doShift "four"
-    , className =? "skype"       --> doShift "four"
-    , isFullscreen --> (doF W.focusDown <+> doFullFloat)
-    ]
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 
-myLayout = avoidStruts (
-    ThreeColMid 1 (3/100) (1/2) |||
-    Tall 1 (3/100) (1/2) |||
-    Mirror (Tall 1 (3/100) (1/2)) |||
-    tabbed shrinkText tabConfig |||
-    Full |||
-    spiral (6/7)) |||
-    noBorders (fullscreenFull Full)
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.LayoutCombinators
+import XMonad.Layout.Maximize
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.SimpleDecoration(shrinkText)
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spacing
+import XMonad.Layout.Dwindle
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.TwoPane
 
-myNormalBorderColor  = "#7c7c7c"
-myFocusedBorderColor = "#ffb6b0"
-myBorderWidth        = 2
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP, additionalMouseBindings)
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.WorkspaceCompare
 
-tabConfig = defaultTheme {
-    activeBorderColor   = "#7c7c7c",
-    activeTextColor     = "#ceffac",
-    activeColor         = "#000000",
-    inactiveBorderColor = "#7c7c7c",
-    inactiveTextColor   = "#eeeeee",
-    inactiveColor       = "#000000"
-}
 
-xmobarTitleColor            = "#ffb6b0"
-xmobarCurrentWorkspaceColor = "#ceffac"
+myTabConfig = def { activeColor = "#556064"
+                  , activeTextColor = "#80FFF9"
+                  , activeBorderColor = "#454948"
+                  , inactiveColor = "#2F3D44"
+                  , inactiveTextColor = "#1ABC9C"
+                  , inactiveBorderColor = "#454948"
+                  , urgentColor = "#FDF6E3"
+                  , urgentTextColor = "#1ABC9C"
+                  , urgentBorderColor = "#268BD2"
+                  , fontName = "xft:Noto Sans CJK:size=10:antialias=true"
+                  }
 
-myModMask = mod4Mask
+myLayout = avoidStruts $
+  -- noBorders (fullscreenFull Full)
+  noBorders Full
+  ||| noBorders (tabbed shrinkText myTabConfig)
+  ||| tiled
+  ||| Mirror tiled
+  ||| twopane
+  ||| Mirror twopane
+  ||| ThreeColMid 1 (3/100) (1/2)
+  ||| emptyBSP
+  ||| Spiral L XMonad.Layout.Dwindle.CW (3/2) (11/10)
+  where
+     -- The last parameter is fraction to multiply the slave window heights
+     -- with. Useless here.
+     tiled = spacing 3 $ ResizableTall nmaster delta ratio []
+	 -- In this layout the second pane will only show the focused window.
+     twopane = spacing 3 $ TwoPane delta ratio
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+myPP = def { ppCurrent = xmobarColor "#1ABC9C" "" . wrap "[" "]"
+           , ppTitle = xmobarColor "#1ABC9C" "" . shorten 60
+           , ppVisible = wrap "(" ")"
+           , ppUrgent  = xmobarColor "red" "yellow"
+           , ppSort = getSortByXineramaPhysicalRule def
+           }
+
+myManageHook = composeAll [ isFullscreen --> doFullFloat ]
 
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- launch a terminal
     [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-    , ((modMask .|.  shiftMask, xK_l), spawn myScreensaver)
-    , ((modMask, xK_p), spawn myLauncher)
---    , ((modMask .|. shiftMask, xK_p), spawn "select-screenshot")
---    , ((modMask .|. controlMask .|. shiftMask, xK_p), spawn "screenshot")
---
-    , ((0, xF86XK_AudioMute), spawn "amixer -q set Master toggle")
-    , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5%-")
-    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5%+")
-    , ((0, xF86XK_MonBrightnessUp), spawn "~/.xmonad/bin/brightness +2")
-    , ((0, xF86XK_MonBrightnessDown), spawn "~/.xmonad/bin/brightness -2")
---    , ((0, 0x1008FF2C), spawn "eject -T")
---
+    -- lock the screen
+    , ((modMask .|.  shiftMask, xK_l), spawn "/usr/bin/xscreensaver-command --lock")
+    -- close the focused window
     , ((modMask .|. shiftMask, xK_c), kill)
-    , ((modMask, xK_space), sendMessage NextLayout)
-    , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
-    , ((modMask, xK_n), refresh)
-    , ((modMask, xK_Tab), windows W.focusDown)
-    , ((modMask, xK_j), windows W.focusDown)
-    , ((modMask, xK_k), windows W.focusUp)
-    , ((modMask, xK_m), windows W.focusMaster)
-    , ((modMask, xK_Return), windows W.swapMaster)
-    , ((modMask .|. shiftMask, xK_j), windows W.swapDown)
-    , ((modMask .|. shiftMask, xK_k), windows W.swapUp)
-    , ((modMask, xK_h), sendMessage Shrink)
-    , ((modMask, xK_l), sendMessage Expand)
-    , ((modMask, xK_t), withFocused $ windows . W.sink)
-    , ((modMask, xK_comma), sendMessage (IncMasterN 1))
-    , ((modMask, xK_period), sendMessage (IncMasterN (-1)))
+    -- destroy the entire session
     , ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+    -- Rotate through the next layout
+    , ((modMask, xK_space), sendMessage NextLayout)
+    -- Reste the layouts on the current workspace to default
+    , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+    -- Change the layout to fullscreen
+    , ((modMask .|. shiftMask, xK_f), sendMessage $ JumpToLayout "Full")
+    -- Resize viewed windows to the correct size
+    , ((modMask, xK_n), refresh)
+    -- Move foucs to the next window
+    , ((modMask, xK_Tab), windows W.focusDown)
+    -- Move focus to the previous window
+    , ((modMask .|. shiftMask, xK_Tab), windows W.focusUp)
+    -- Move focus to the master window
+    , ((modMask, xK_m), windows W.focusMaster)
+    -- Swap the focused window with the master window
+    , ((modMask, xK_Return), windows W.swapMaster)
+    -- Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_j), windows W.swapDown)
+    -- Swap the focused window with the previous window
+    , ((modMask .|. shiftMask, xK_k), windows W.swapUp)
+    -- Shrink the master window
+    , ((modMask, xK_h), sendMessage Shrink)
+    -- Expand the master window
+    , ((modMask, xK_l), sendMessage Expand)
+    -- Shrink ratio between secondary panes for ResizeableTall layout
+    , ((modMask .|. shiftMask, xK_h), sendMessage MirrorShrink)
+    -- Expand ratio between secondary panes for ResizeableTall layout
+    , ((modMask .|. shiftMask, xK_h), sendMessage MirrorExpand)
+    -- Push window back into tiling
+    , ((modMask, xK_t), withFocused $ windows . W.sink)
+    -- Increase number of window in master area
+    , ((modMask, xK_comma), sendMessage (IncMasterN 1))
+    -- Decrease number of window in master area
+    , ((modMask, xK_period), sendMessage (IncMasterN (-1)))
+    -- Recompile xmonad config and refresh the setup
     , ((modMask, xK_q), restart "xmonad" True)
+    -- Toggles the status bar gap
+    , ((modMask, xK_b), sendMessage ToggleStruts)
+    -- Take a screenshot
+    , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+	-- Open rofi in combi mode
+    , ((modMask, xK_p), spawn "rofi -combi-modi window,run,drun -show combi -modi combi")
     ] ++
+    -- Sends the focused window to the given workspace
     [ ((m .|. modMask, k), windows $ f i) | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9], (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)] ] ++
+    -- Switches between physical screens
     [ ((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f)) | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..], (f, m) <- [(W.view, 0), (W.shift, shiftMask)] ] ++
+    -- Swaps the current workspace with the given workspace
     [ ((modMask .|. controlMask, k), windows $ swapWithCurrent i) | (i, k) <- zip myWorkspaces [xK_1 .. xK_9] ]
 
-myFocusFollowsMouse :: Bool
-myFocusFollowsMouse = True
-
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- Take the focused window from tiling
     [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    -- Swap the focused window with master
     , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    -- Resize the focused window with mouse
     , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
     ]
 
-myStartupHook = do
-    E.ewmhDesktopsStartup
-    return ()
-
---myEventHook = E.ewmhDesktopsEventHook <+> E.fullscreenEventHook <+> fullscreenEventHook
---myEventHook = E.ewmhDesktopsEventHook
-
 main = do
-    xmproc <- spawnPipe "/usr/local/bin/xmobar ~/.xmobarrc"
-    xmonad $ defaults
-        { manageHook      = manageDocks <+> manageHook defaultConfig
-        , handleEventHook = handleEventHook defaultConfig <+> E.ewmhDesktopsEventHook <+> E.fullscreenEventHook <+> docksEventHook
-        , logHook         = dynamicLogWithPP $ xmobarPP
-            { ppOutput  = hPutStrLn xmproc
-            , ppTitle   = xmobarColor xmobarTitleColor "" . shorten 20
-            , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
-            , ppSep     = "  "
-            }
-        }
-
-
-defaults = E.ewmh $ defaultConfig
-    { terminal           = myTerminal
-    , focusFollowsMouse  = myFocusFollowsMouse
-    , borderWidth        = myBorderWidth
-    , modMask            = myModMask
-    , workspaces         = myWorkspaces
-    , normalBorderColor  = myNormalBorderColor
-    , focusedBorderColor = myFocusedBorderColor
-    , keys               = myKeys
-    , mouseBindings      = myMouseBindings
-    , layoutHook         = smartBorders $ myLayout
-    , manageHook         = myManageHook
---    , handleEventHook    = myEventHook
-    , startupHook        = myStartupHook
-    }
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ ewmh def
+        { modMask = mod4Mask
+        , keys = myKeys
+        , mouseBindings = myMouseBindings
+        , focusFollowsMouse = True
+        , terminal = "/home/parv/.local/kitty.app/bin/kitty"
+        , manageHook = manageDocks <+> myManageHook
+        , layoutHook = myLayout
+        , handleEventHook = handleEventHook def <+> docksEventHook
+        , logHook = dynamicLogWithPP myPP { ppOutput = hPutStrLn xmproc } >> historyHook
+        , normalBorderColor = "#2F3D44"
+        , focusedBorderColor = "#1ABC9C"
+        } `additionalKeysP`
+        [ ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
+        , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5")
+		, ("<XF86AudioMute>", spawn "amixer -q set Master togglemute")
+        , ("<XF86AudioLowerVolume>", spawn "amixer -c 0 -q set Master 2dB-")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer -c 0 -q set Master 2dB+")
+        , ("<XF86AudioPlay>", spawn "playerctl play-pause")
+        , ("<XF86AudioNext>", spawn "playerctl next")
+        , ("<XF86AudioPrev>", spawn "playerctl prev")
+        ]
